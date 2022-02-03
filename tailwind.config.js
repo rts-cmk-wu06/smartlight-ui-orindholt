c6d06fc00cbc9e136df9e01fe9c31d6c8eb4bb07
orindholt <83012141+orindholt@users.noreply.github.com>
2022-02-03 16:18:35 +0100

    Add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#393939",
+        blue: "#0A4DA2",
+        darkBlue: "#093B7B",
+        gray: "#F6F8FB",
+        orange: "#FFA939",
+        salmon: "#FF9898",
+        green: "#94EB9E",
+        lightBlue: "#94CAEB",
+        purple: "#A594EB",
+        pink: "#DE94EB",
+        yellow: "#EBD094",
+      },
+      maxWidth: {
+        sm: "375px"
+      }
+    },
   },
   plugins: [],
 }
