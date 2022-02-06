19083fc2c867551074e6f24c6067f3faf3412457
Oliver Rindholt <U75087@rts-365.dk>
2022-02-07 00:31:55 +0100

    Fix shadow and header in component

--- src/Components/RoomLink.js
+++ src/Components/RoomLink.js
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 
 const RoomLink = ({room, icon, lights}) => {
   return (
-    <Link 
+    <li>
+      <Link 
       to={room} 
-      className="bg-white py-6 px-5 rounded-3xl w-[150px] h-[150px] flex flex-col justify-between"
+      className="bg-white py-6 px-5 rounded-3xl w-[150px] h-[150px] flex flex-col justify-between shadow-md"
     >
       {icon}
-      <p>{room}</p>
+      <h3>{room}</h3>
       <p className="text-orange">{lights} Lights</p>
     </Link>
+    </li>
   );
 }
  
