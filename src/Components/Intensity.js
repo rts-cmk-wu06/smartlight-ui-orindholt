1f3af05d07acad3958fafeda6cd83f16867fb5ce
Oliver Rindholt <U75087@rts-365.dk>
2022-02-07 00:30:48 +0100

    Fix context to intensity component

--- src/Components/Intensity.js
+++ src/Components/Intensity.js
@@ -1,17 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { ReactComponent as BulbOffIcon } from '../assets/svg/bulbOff.svg';
 import { ReactComponent as BulbOnIcon } from '../assets/svg/bulbOn.svg';
+import { BulbContext } from '../Utility/BulbContext';
+import DetailTitle from './DetailTitle';
+
 
 const Intensity = () => {
-  const [state, setState] = useContext(IntensityContext);
+  const { sliderVal, setSliderVal } = useContext(BulbContext);
+
+  useEffect(()=>{
+    setSliderVal(sliderVal);
+  }, []);
+
   return (
-    <div className="flex items-center">
-      <BulbOffIcon />
-      <input className="w-full h-[1px] outline-none bg-darkGray appearance-none thumb mx-4" type="range" min="0" max="100" 
-        onChange={(e) => setState(e.target.value)} 
-      />
-      <BulbOnIcon />
-    </div>
+    <section>
+      <DetailTitle title="Intensity" />
+      <div className="flex items-center">
+        <BulbOffIcon className="w-10" />
+        <input className="w-full h-[1px] outline-none bg-darkGray appearance-none thumb mx-4" type="range" min="0" max="100" 
+          value={sliderVal ? sliderVal : 100}
+          onChange={(e) => setSliderVal(e.target.value)} 
+        />
+        <BulbOnIcon className="w-10" />
+      </div>
+    </section>
   );
 }
  
