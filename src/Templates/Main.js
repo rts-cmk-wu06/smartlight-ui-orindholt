0fad513661825f10c92e288660922aa93ba193bf
Oliver Rindholt <U75087@rts-365.dk>
2022-02-07 00:33:05 +0100

    Add main template

---
+++ src/Templates/Main.js
@@ -0,0 +1,31 @@
+import { useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const Main = ({children}) => {
+  const url = useLocation();
+  const variants = {
+    initial: {height: 580, transition: {
+      type: "tween",
+      duration: 1
+    }},
+    animate: {
+      height: 480, transition: {
+        type: "tween",
+        duration: 1
+      }
+    },
+  }
+
+  return (
+    <motion.main 
+      variants={variants}
+      initial="initial"
+      animate={url.pathname.length > 2 ? "animate" : "initial"}
+      className="bg-gray p-6 rounded-t-3xl h-[580px]"
+    >
+      {children}
+    </motion.main>
+  );
+}
+ 
+export default Main;
