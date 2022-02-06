cf57ad0f90fe815bc66a651618932e1dd1b24d3b
Oliver Rindholt <U75087@rts-365.dk>
2022-02-07 00:32:21 +0100

    Add motion to detail component

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -1,15 +1,31 @@
-import { useContext } from 'react';
-import { useParams } from 'react-router-dom';
 import Intensity from '../Components/Intensity';
+import { motion } from 'framer-motion';
+import ColorPicker from '../Components/ColorPicker';
+import Scenes from '../Components/Scenes';
+
+const fadeVariants = {
+  hidden :{opacity: 0},
+  shown: {
+    opacity: 1, 
+    transition: {
+      duration: 1
+    }
+  },
+  exit: {
+    opacity: 0,
+  }
+}
 
 const Detail = () => {
-  const { id } = useParams();
-  console.log(id);
   return (
-    <>
-      <h2>Intensity</h2>
+    <motion.div 
+      variants={fadeVariants} animate="shown" initial="hidden" exit="exit"
+      className="flex flex-col justify-between h-full pb-12"
+    >
       <Intensity />
-    </>
+      <ColorPicker />
+      <Scenes />
+    </motion.div>
   );
 }
  
