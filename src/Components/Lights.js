7e5eee7493953868a96c98ef4570acd7d5b634b3
Oliver Rindholt <U75087@rts-365.dk>
2022-02-08 14:22:10 +0100

    Add lights component

---
+++ src/Components/Lights.js
@@ -0,0 +1,14 @@
+import { motion } from "framer-motion";
+
+export const Lights = ({amount}) => {
+  return (
+    <motion.h3 
+      className="text-yellow"
+      initial={{y: -20, opacity: 0}}
+      animate={{y: 5, opacity: 1}}
+      transition={{duration: 1, delay: 1}}
+    >
+      {amount}{' '}{amount ? "Lights" : "Light"}
+    </motion.h3>
+  );
+}
