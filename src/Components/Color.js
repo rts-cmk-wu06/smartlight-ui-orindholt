a1434b0af5f5beeb6b1e52ea51e9a52e91550f0c
Oliver Rindholt <U75087@rts-365.dk>
2022-02-08 14:21:11 +0100

    Replace data attribute to state

--- src/Components/Color.js
+++ src/Components/Color.js
@@ -1,22 +1,21 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { BulbContext } from '../Utility/BulbContext';
 import { motion } from "framer-motion";
 
 const Color = ({color, index}) => {
   const {setColorPick} = useContext(BulbContext);
+  const [crntColor, setCrntColor] = useState(null);
   const value = parseInt(`-${10*index/2}`);
+  useEffect(()=>{setCrntColor(color)}, [color]);
 
   return (
     <motion.li
       initial={{x: value}}
       animate={{x: 0}}
       transition={{duration: 1, delay: 1}}
-      datacolor={color} 
       className="w-7 h-7 rounded-full cursor-pointer" 
       style={{backgroundColor: color}}
-      onClick={(e)=>{
-        setColorPick(e.target.getAttribute("datacolor"));
-      }}
+      onClick={()=>setColorPick(crntColor)}
     >
     </motion.li>
   );
