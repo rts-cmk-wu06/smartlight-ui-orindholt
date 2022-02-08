60ff77293e5f51e56f928373307b2e096bd0df03
Oliver Rindholt <U75087@rts-365.dk>
2022-02-08 14:22:48 +0100

    Remove unnessecary useEffect

--- src/Components/Intensity.js
+++ src/Components/Intensity.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { ReactComponent as BulbOffIcon } from '../assets/svg/bulbOff.svg';
 import { ReactComponent as BulbOnIcon } from '../assets/svg/bulbOn.svg';
 import { BulbContext } from '../Utility/BulbContext';
@@ -8,16 +8,12 @@ import DetailTitle from './DetailTitle';
 const Intensity = () => {
   const { sliderVal, setSliderVal } = useContext(BulbContext);
 
-  useEffect(()=>{
-    setSliderVal(sliderVal);
-  }, []);
-
   return (
     <section>
       <DetailTitle title="Intensity" />
       <div className="flex items-center">
         <BulbOffIcon className="w-10" />
-        <input className="w-full h-[1px] outline-none bg-darkGray appearance-none thumb mx-4" type="range" min="0" max="100" 
+        <input className="slider" type="range" min="0" max="100" 
           value={sliderVal ? sliderVal : 100}
           onChange={(e) => setSliderVal(e.target.value)} 
         />
