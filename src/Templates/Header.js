ee121751eba626bc0577759194504a9cac4d34b6
Oliver Rindholt <U75087@rts-365.dk>
2022-02-08 14:22:00 +0100

    Add light and light scenes to header

--- src/Templates/Header.js
+++ src/Templates/Header.js
@@ -2,26 +2,32 @@ import { useContext } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import HeaderContent from '../Components/HeaderContent';
 import Lamp from '../Components/Lamp';
+import { Lights } from '../Components/Lights';
 import PageTitle from '../Components/PageTitle';
 import ProfileIcon from "../Components/ProfileIcon";
 import { BulbContext } from '../Utility/BulbContext';
+import LightScenes from './LightScenes';
 
 const Header = () => {
-  const { colorPick } = useContext(BulbContext);
-  const location = useLocation();
+  const { colorPick, lightAmount } = useContext(BulbContext);
+  const { pathname } = useLocation();
   
   return (
-    <header className="p-6 pt-[60px] pb-0 relative">
+    <header className="p-6 pt-[50px] pb-0 relative">
       <Routes>
         <Route path="/" element={
           <HeaderContent>
-            <PageTitle title={location.pathname} />
+            <PageTitle title={pathname} />
             <ProfileIcon />
           </HeaderContent>
         }/>
         <Route path="/:id" element={
           <HeaderContent>
-            <PageTitle title={location.pathname} />
+            <div>
+              <PageTitle title={pathname} />
+              <Lights amount={lightAmount} />
+              <LightScenes />
+            </div>
             <Lamp color={colorPick} />
           </HeaderContent>
         }/>
